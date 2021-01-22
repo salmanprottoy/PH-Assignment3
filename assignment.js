0efe3a97a1ecfec0b12a75483e2f1d5aff9bda8f
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-01-22 17:20:43 +0600

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -21,3 +21,36 @@ function budgetCalculator(watch, phone, laptop) {
         return "Invalid Input!";
     }
 }
+
+// Problem 3 - hotel cost calculator
+function hotelCost(numberOfDays) {
+    var first10DaysCostRate     = 100;
+    var second10DaysCostRate    = first10DaysCostRate - 20;
+    var restOfAllDaysCostRate   = 50;
+    var totalCost               = 0 ;
+    if(numberOfDays >= 0){
+        if(numberOfDays <= 10){
+            totalCost = first10DaysCostRate * numberOfDays;
+            return totalCost;
+        }
+        else if(numberOfDays <= 20){
+            var remainingDays       = numberOfDays - 10;
+            var first10DaysCost     = 10 * first10DaysCostRate;
+            var remainingDaysCost   = remainingDays * second10DaysCostRate;
+            totalCost = first10DaysCost + remainingDaysCost;
+            return totalCost;
+        } 
+        else{
+            var remainingDays       = numberOfDays - 20;
+            var first10DaysCost     = 10 * first10DaysCostRate;
+            var second10DaysCost    = 10 * second10DaysCostRate;
+            var remainingDaysCost   = remainingDays * restOfAllDaysCostRate;
+            totalCost = first10DaysCost + second10DaysCost + remainingDaysCost
+            return totalCost;
+        }
+    }
+    else{
+        return "Invalid Input!";
+    }
+}
+
