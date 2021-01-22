12d376433cb89f721e61b4e0c9dfc966f023b069
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-01-22 16:14:34 +0600

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,1 +1,12 @@
 // https://github.com/salmanprottoy/PH-Assignment3
+
+// Problem 1 - kilometer to meter conversion
+function kilometerToMeter(kilometer) {
+    if (kilometer >= 0) {
+        // 1000 meter = 1 kilometer
+        return kilometer*1000;
+    }
+    else {
+        return "Invalid Input!";
+    }
+}
