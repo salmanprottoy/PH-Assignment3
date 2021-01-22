8ce6ea23da5c721f0f68bb7d5d389e9b49fcabb8
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-01-22 16:31:15 +0600

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -4,9 +4,20 @@
 function kilometerToMeter(kilometer) {
     if (kilometer >= 0) {
         // 1000 meter = 1 kilometer
-        return kilometer*1000;
+        return (kilometer*1000);
     }
     else {
         return "Invalid Input!";
+    }
+}
+
+// Problem 2 - budget calculator
+function budgetCalculator(watch, phone, laptop) {
+    if ((watch >= 0) && (phone >= 0) && (laptop >= 0)) {
+        // watch price = 50, phone price = 100, laptop price = 500
+        return ((50 * watch) + (100 * phone) + (500 * laptop));
+    }
+    else {
+        return "Invalid Input";
     }
 }
